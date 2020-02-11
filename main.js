4c0805e7e6902b1f4d5ae54424955bfba179916a
xiaoqi <253506624@qq.com>
2020-02-11 10:42:22 +0800

    third

--- main.js
+++ main.js
@@ -1,21 +1,35 @@
 const {app, BrowserWindow,ipcMain} = require('electron');
-app.on('ready', () => {
-    const mainWindow = new BrowserWindow({
-        width: 800,
-        height: 600,
-        webPreferences:{
-            nodeIntegration: true
-        }
-    });
-    mainWindow.loadFile('./renderer/index.html');
-    ipcMain.on('add-music',()=>{
-        const addWindow = new BrowserWindow({
-            width: 600,
-            height: 400,
+
+// 封装一个创建窗口的类（代码复用）
+class AddWindow extends BrowserWindow {
+    constructor(config, url){
+        const baseConfig = {
+            width: 800,
+            height: 600,
             webPreferences:{
-                nodeIntegration: true
+            nodeIntegration: true
             }
-        });
-        addWindow.loadFile('./renderer/add.html');
+        }
+         // 最终的Config  
+        const finallConfig = Object.assign(baseConfig, config)
+        // 也可以这样写： const finallConfig = {...baseConfig, ...config}
+        // 调用父类的方法，即创建一个窗口
+        super(finallConfig);
+        // 加载相应的HTML文件
+        this.loadFile(url);
+        // 优雅地显示窗口
+        this.once('ready-to-show', () => {
+            this.show();
+        })
+    }  
+}
+app.on('ready', () => {
+    const mainWindow = new AddWindow({}, './renderer/index.html');
+    ipcMain.on('add-music',()=>{
+        const addWindow = new AddWindow({
+            width: 600,
+            height: 400, 
+            parent:mainWindow
+        },'./renderer/add.html' );
     })
 })
