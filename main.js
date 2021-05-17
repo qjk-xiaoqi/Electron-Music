6c0ef5bcbaa819f5e172629f8a9a1b6919e176b1
wanmian <wanmian@bytedance.com>
2021-05-17 13:15:04 +0800

    fix: 文件修改

--- main.js
+++ main.js
@@ -1,84 +1,85 @@
-const {app, BrowserWindow,ipcMain,dialog} = require('electron');
-const musicStore = require('./renderer/musicStore.js');
+const { app, BrowserWindow, ipcMain, dialog } = require('electron')
+const musicStore = require('./renderer/musicStore.js')
 
-const store = new musicStore();
- 
+const store = new musicStore()
+
 // 程序存放的路径
-// console.log(app.getPath('userData'));
- 
+console.log(app.getPath('userData'))
 
 // 封装一个创建窗口的类（代码复用）
 class AddWindow extends BrowserWindow {
-    constructor(config, url){
-        const baseConfig = {
-            width: 800,
-            height: 600,
-            webPreferences:{
-            nodeIntegration: true
-            }
-        }
-        // 最终的Config  
-        const finallConfig = Object.assign(baseConfig, config)
-        // 也可以这样写： const finallConfig = {...baseConfig, ...config}
-        // 调用父类的方法，即创建一个窗口
-        super(finallConfig);
-        // 加载相应的HTML文件
-        this.loadFile(url);
-        // 优雅地显示窗口
-        this.once('ready-to-show', () => {
-            this.show();
-        })
-    }  
+  constructor(config, url) {
+    const baseConfig = {
+      width: 800,
+      height: 600,
+      webPreferences: {
+        nodeIntegration: true,
+      },
+    }
+    // 最终的Config
+    const finallConfig = Object.assign(baseConfig, config)
+    // 也可以这样写： const finallConfig = {...baseConfig, ...config}
+    // 调用父类的方法，即创建一个窗口
+    super(finallConfig)
+    // 加载相应的HTML文件
+    this.loadFile(url)
+    // 优雅地显示窗口
+    this.once('ready-to-show', () => {
+      this.show()
+    })
+  }
 }
 app.on('ready', () => {
-    const mainWindow = new AddWindow({}, './renderer/index.html');
-    // 当导航完成时发出事件，onload 事件也完成.
-    mainWindow.webContents.on('did-finish-load', () => {
-        // console.log(store.getData());
-        mainWindow.send('renderList', store.getData());
-    })
-    ipcMain.on('add-music',()=>{
-        const addWindow = new AddWindow({
-            width: 600,
-            height: 400, 
-            parent:mainWindow
-        },'./renderer/add.html' );
-    });
-    ipcMain.on('open-music-dialog', (event)=>{
-        dialog.showOpenDialog({
-            // 允许选择文件、允许多选
-            properties: ['openFile', 'multiSelections'],
-            // 指定可选文类型的数组
-            filters: [
-                {name: 'Music', extensions:['mp3']}
-            ]
-        }).then((files) => {
-            // 如果文件存在，传入文件路径，并通知add.js中展示音乐列表
-             if(files) {
-                event.sender.send('show-list',files.filePaths);
-             }
-        }).catch(() => {
-            console.log('error');
-        })
-    });
+  const mainWindow = new AddWindow({}, './renderer/index.html')
+  // 当导航完成时发出事件，onload 事件也完成.
+  mainWindow.webContents.on('did-finish-load', () => {
+    // console.log(store.getData());
+    mainWindow.send('renderList', store.getData())
+  })
+  ipcMain.on('add-music', () => {
+    const addWindow = new AddWindow(
+      {
+        width: 600,
+        height: 400,
+        parent: mainWindow,
+      },
+      './renderer/add.html',
+    )
+  })
+  ipcMain.on('open-music-dialog', (event) => {
+    dialog
+      .showOpenDialog({
+        // 允许选择文件、允许多选
+        properties: ['openFile', 'multiSelections'],
+        // 指定可选文类型的数组
+        filters: [{ name: 'Music', extensions: ['mp3'] }],
+      })
+      .then((files) => {
+        // 如果文件存在，传入文件路径，并通知add.js中展示音乐列表
+        if (files) {
+          event.sender.send('show-list', files.filePaths)
+        }
+      })
+      .catch(() => {
+        console.log('error')
+      })
+  })
 
-    // 监听导入音乐事件
-    ipcMain.on('import-musicData', (event, data)=>{
-       const updataMusic = store.addData(data).getData();
-       mainWindow.send('renderList', updataMusic )
-    });
-    ipcMain.on('notData',()=>{
-        dialog.showMessageBox({
-            type: "info",
-            title: '提示',
-            message: '请选择要导出的音乐文件'
-        })
-    });
-    // 监听删除音乐事件
-    ipcMain.on('delete-music',(event,data) => {
-        const updataMusic = store.delData(data).getData();
-        mainWindow.send('renderList', updataMusic )
+  // 监听导入音乐事件
+  ipcMain.on('import-musicData', (event, data) => {
+    const updataMusic = store.addData(data).getData()
+    mainWindow.send('renderList', updataMusic)
+  })
+  ipcMain.on('notData', () => {
+    dialog.showMessageBox({
+      type: 'info',
+      title: '提示',
+      message: '请选择要导出的音乐文件',
     })
+  })
+  // 监听删除音乐事件
+  ipcMain.on('delete-music', (event, data) => {
+    const updataMusic = store.delData(data).getData()
+    mainWindow.send('renderList', updataMusic)
+  })
 })
-
- 
